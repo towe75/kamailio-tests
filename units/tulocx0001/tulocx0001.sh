ca2621829615dc1423ee10f3a1f7026e986e3642
Thomas Weber <thomas.weber@pascom.net>
2018-05-31 22:54:18 +0200

    Experimental setup/teardown api

--- units/tulocx0001/tulocx0001.sh
+++ units/tulocx0001/tulocx0001.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-. ../../etc/config
-. ../../libs/utils
+. $TESTROOT/etc/config
+. $TESTROOT/libs/utils
 
-echo
-echo "--- start with default config"
-${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no
-ret=$?
-sleep 1
-if [ ! "$ret" -eq 0 ] ; then
-    exit $ret
-fi
-echo
-echo "--- start sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1"
-sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1 2>&1 | tee /tmp/kamailio-tulocx0001.log &
-sleep 1
-echo
-echo "--- grep output"
-echo
-grep "usrloc for alice completed successful" /tmp/kamailio-tulocx0001.log
-ret=$?
-if [ ! "$ret" -eq 0 ] ; then
-    exit 1
-fi
-kill_pidfile ${KAMPID}
+setup() {
+    echo
+    echo "--- start with default config"
+    ${KAMBIN} -P ${KAMPID} -w ${KAMRUN} -Y ${KAMRUN} -a no
+    sleep 1
+}
 
-exit $ret
+teardown() {
+    kill_pidfile ${KAMPID}
+}
+
+test_usrloc_alice() {
+    echo
+    echo "--- start sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1"
+    sipsak -vvv -U -I -H 127.0.0.1 -s sip:alice@127.0.0.1 2>&1 | tee /tmp/kamailio-tulocx0001.log &
+    sleep 1
+    echo
+    echo "--- grep output"
+    echo
+    grep "usrloc for alice completed successful" /tmp/kamailio-tulocx0001.log
+}
+
+run_test test_usrloc_alice
